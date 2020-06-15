aab8c79a8e78afec5dcdb2a428a97623d285987a
dogsteven <caigicungduoc23@gmail.com>
2020-06-15 20:22:43 +0700

    simple api

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import router from './router'
 import cors from 'cors'
 
 let app = express()
@@ -6,5 +7,6 @@ let app = express()
 app.use(express.json())
 app.use(cors({ origin: true }))
 app.use(express.static('static'))
+app.use(router)
 
 app.listen(80)
--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 import express from 'express'
 let router = express.Router()
 
-
-
+router.get('/:name', (req, res) => {
+    res.json({
+        name: req.params.name
+    })
+})
 
 export default router
